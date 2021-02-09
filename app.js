c315e23fd180169f22bc913cc91283735f00329d
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-02-09 12:44:21 +0100

    push test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json()); // middleware for sending request
 
 const articles = JSON.parse(
     fs.readFileSync(`${__dirname}/data/articles.json`)
@@ -15,6 +16,23 @@ app.get('/articles', (req, res) => {
             articles
         }
     })
+});
+
+app.post('/articles', (req, res) => {
+    const newId = JSON.stringify((articles.length -1) + 1);
+    console.log(articles.length);
+    const newArticle = Object.assign({ id: newId }, req.body);
+
+    articles.push(newArticle);
+
+    fs.writeFile(`${__dirname}/data/articles.json`, JSON.stringify(articles), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                article: newArticle
+            }
+        });
+    });
 })
 
 const port = 3001;
