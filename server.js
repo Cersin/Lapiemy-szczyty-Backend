c162ca692dba2cf0da57a595ea24904b4e0edd3a
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-02-22 09:26:45 +0100

    test mongoose connection

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
 
 dotenv.config({path: './config.env'});
-// console.log(process.env);
-console.log(process.env.NODE_ENV);
+
+const DB = process.env.DATABESE_LOCAL;
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Połączono z bazą danych');
+}).catch((err) => {
+    console.log(err);
+})
+// console.log(process.env.NODE_ENV);
 // Listen to server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
