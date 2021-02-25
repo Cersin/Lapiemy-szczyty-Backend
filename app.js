7879ff0796e5f8f6dacd07e4c9f40dfe4a8e96d9
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-02-25 11:49:40 +0100

    global error handler

--- app.js
+++ app.js
@@ -16,11 +16,26 @@ app.use(express.json()); // middleware for sending request
 app.use('/articles', articleRouter);
 app.use('/admin', adminRouter);
 
+// Wrong path handler
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Nie można znaleźć ${req.originalUrl}`
-    });
+    const err = new Error(`Nie można znaleźć ${req.originalUrl}`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err);
 });
+
+// Global error handler
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+
+    next();
+})
 
 module.exports = app;
