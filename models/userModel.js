6446763ca144a632d4eb3209685ac69ac9eac525
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-02-26 12:14:04 +0100

    add user with bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -19,12 +20,24 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Prosze potwierdź hasło'],
         validate: {
-            validator: function(val) {
+            validator: function (val) {
                 return val === this.password;
             },
             message: 'Twoje hasła nie są takie same'
         }
     }
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    // Hash password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete passwordConfirm
+    this.passwordConfirm = undefined;
+
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
