7eca95200af80adfb72cdbe9b5379aa7c96bdf94
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-03-30 10:53:38 +0200

    http

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -32,7 +32,7 @@ exports.responseImage = async (req, res) => {
     const imageUrl = req.file.path.replace(/\\/g, "/").substring("public".length);
     try {
         res.status(200).json({
-            url: `${req.hostname}:${process.env.PORT}${imageUrl}`
+            url: `http://${req.hostname}:${process.env.PORT}${imageUrl}`
         });
     } catch (err) {
         res.status(404).json({
