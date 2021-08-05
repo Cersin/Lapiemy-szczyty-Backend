9092e365de03086a2b09487a76e1cbd54b3b6cff
Szymon Wójcik <cersin96@gmail.com>
2021-08-05 14:08:04 +0200

    public changes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hpp = require('hpp');
 const cors = require('cors');
 const history = require('connect-history-api-fallback');
 const compression = require('compression');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const appError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -20,13 +22,14 @@ const categoryRouter = require('./routes/categoryRoutes');
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:8081/"
+    origin: "http://localhost:3001"
 };
 
 app.use(cors());
 
 // history mode to work
 app.use(history());
+app.use(express.static('public'));
 
 // Set security HTTP Headers
 app.use(helmet());
@@ -60,7 +63,7 @@ app.use(hpp());
 // serve static files
 app.use(express.static(`${__dirname}/public`));
 
-const path = __dirname + '/views';
+const path = __dirname + '/public';
 app.use(express.static(path));
 
 // use routes
@@ -78,4 +81,39 @@ app.all('*', (req, res, next) => {
 // Global error handler
 app.use(globalErrorHandler);
 
-module.exports = app;
+
+process.on('uncaughtException', (err) => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION! Shutting down.');
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE_ATLAS;
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Połączono z bazą danych');
+});
+
+// console.log(process.env.NODE_ENV);
+// Listen to server
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log(err.name, err.message);
+    console.log('Błąd servera! Zamykam serwer.');
+    server.close(() => {
+        process.exit(1);
+    });
+});
--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-process.on('uncaughtException', (err) => {
-    console.log(err.name, err.message);
-    console.log('UNCAUGHT EXCEPTION! Shutting down.');
-    server.close(() => {
-        process.exit(1);
-    });
-});
-
-const app = require('./app');
-
-dotenv.config({path: './config.env'});
-
-const DB = process.env.DATABASE_ATLAS;
-
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Połączono z bazą danych');
-});
-
-// console.log(process.env.NODE_ENV);
-// Listen to server
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-});
-
-process.on('unhandledRejection', (err) => {
-    console.log(err.name, err.message);
-    console.log('Błąd servera! Zamykam serwer.');
-    server.close(() => {
-        process.exit(1);
-    });
-});
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+//
+// process.on('uncaughtException', (err) => {
+//     console.log(err.name, err.message);
+//     console.log('UNCAUGHT EXCEPTION! Shutting down.');
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
+//
+// const app = require('./app');
+//
+// dotenv.config({path: './config.env'});
+//
+// const DB = process.env.DATABASE_ATLAS;
+//
+// mongoose.connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Połączono z bazą danych');
+// });
+//
+// // console.log(process.env.NODE_ENV);
+// // Listen to server
+// const port = process.env.PORT || 3000;
+// const server = app.listen(port, () => {
+//     console.log(`App running on port ${port}`);
+// });
+//
+// process.on('unhandledRejection', (err) => {
+//     console.log(err.name, err.message);
+//     console.log('Błąd servera! Zamykam serwer.');
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
